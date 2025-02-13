619436c39bd2e0ceaf7595eb127f84327444bf88
FabriceMru <jean.nadal@dci-student.org>
2025-02-13 10:20:10 +0100

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/schiffe-versenken/', // Name des Repositories
+  plugins: [react()],
+  base: './', // Wichtig für Vercel
 });
